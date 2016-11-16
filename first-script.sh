1de56a2515f74cc04947d6ed10f7e2a7c53093d3
Daniel Isaac Khan Ramiro <di.khan.r@gmail.com>
2016-11-16 22:21:01 +0000

    Fix double square bracket issue

--- first-script.sh
+++ first-script.sh
@@ -2,11 +2,9 @@
 
 echo "first-script.sh"
 
-if [[ "$TRAVIS_BRANCH" == "master" ]];
-then
+if [ "$TRAVIS_BRANCH" == "master" ];then
   echo "Building master branch first..."
-else if [[ "$TRAVIS_PULL_REQUEST" == "true" ]];
-    then
+else if [ "$TRAVIS_PULL_REQUEST" == "true" ];then
         echo "Building PR request first..."
     fi
 fi
